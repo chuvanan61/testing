932df6f4499d5ed008acacbdb781b6f427f55b9d
dinh61 <dinh61@rogers.com>
2020-10-06 14:23:00 -0400

    new test

--- testing/src/test/Hello.java
+++ testing/src/test/Hello.java
@@ -1,10 +1,12 @@
 package test;
 
 public class Hello {
-
+// test
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello");
+		System.out.println("Hello");
+
 
 
 	}
